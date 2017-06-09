104b3b3ec77bc0d6bb1f4a42371fad8ad42de4a6
root <mayankdighe@gmail.com>
2017-06-09 12:29:57 +0530

    Code cleanup

--- server/cgi-bin/test.py
+++
@@ -1,6 +0,0 @@
-#!/home/mayank/Envs/meetup/bin/python
-
-print('Content-type: text/html\n')
-for i in range(1, 11):
-    print("<p>", 2 * i, "</p>")
-print("<p>Hello you have just calculated the first 10 multiples of 2 on the server. Sup Chots<p>")
